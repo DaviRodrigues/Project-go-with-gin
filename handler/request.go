7ad94c70cd3f5a11f417a65dc2726f582fe8ee0e
Davi <oversouls11@gmail.com>
2025-08-16 01:25:18 -0300

    feat: add request and response helpers

---
+++ handler/request.go
@@ -0,0 +1,39 @@
+package handler
+
+import "fmt"
+
+func errParamIsRequired(name, typ string) error {
+	return fmt.Errorf("param: %s (type: %s) is required", name, typ)
+}
+
+// CreateOpening
+type CreateOpeningRequest struct {
+	Role     string `json:"role"`
+	Company  string `json:"company"`
+	Location string `json:"location"`
+	Remote   *bool  `json:"remote"`
+	Link     string `json:"link"`
+	Salary   uint64 `json:"salary"`
+}
+
+func (c *CreateOpeningRequest) Validate() error {
+	if c.Role == "" {
+		return errParamIsRequired("role", "string")
+	}
+	if c.Company == "" {
+		return errParamIsRequired("company", "string")
+	}
+	if c.Location == "" {
+		return errParamIsRequired("location", "string")
+	}
+	if c.Remote == nil {
+		return errParamIsRequired("remote", "bool")
+	}
+	if c.Link == "" {
+		return errParamIsRequired("link", "string")
+	}
+	if c.Salary == 0 {
+		return errParamIsRequired("salary", "uint64")
+	}
+	return nil
+}
