91b27f534e7885f8c2157ecc161888749e44908e
Davi <oversouls11@gmail.com>
2025-08-15 23:46:43 -0300

    feat: add opening schema and loggers

--- main.go
+++ main.go
@@ -1,8 +1,23 @@
 package main
 
-import "github.com/DaviRodrigues/Project-go-with-gin/router"
+import (
+	"github.com/DaviRodrigues/Project-go-with-gin/config"
+	"github.com/DaviRodrigues/Project-go-with-gin/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+
+	// Intialize Configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		panic(err)
+	}
 
 	router.Initialize()
 }
