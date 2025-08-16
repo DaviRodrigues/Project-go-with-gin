c198ddffc7833e4e8290bf65cb83ddc26a91ff3e
Davi <oversouls11@gmail.com>
2025-08-15 22:23:06 -0300

    feat: add routes

--- router/router.go
+++ router/router.go
@@ -3,13 +3,12 @@ package router
 import "github.com/gin-gonic/gin"
 
 func Initialize() {
+	// Initialize router
 	r := gin.Default()
 
-	r.GET("/ping", func(ctx *gin.Context) {
-		ctx.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	// Intialize Routes
+	initializeRoutes(r)
 
+	// Run server
 	r.Run(":8081")
 }
